30d4830b7c28de3533d53b3374da97bbd6a3578a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-14 23:26:12 +0000

    a bit more

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.HUnit
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+foo :: Int -> (Int, Int)
+foo x = (1, x)
+
+test1 = TestCase (assertEqual "for (foo 3)," (1,2) (foo 3))
