892fb16e368b49b2faced36b91ccd4b6e828f9eb
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-15 11:03:37 +0000

    fukcing haskell arggg

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
-import Lib
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Data.ByteString.Lazy.Char8 as L8
+import           Network.HTTP.Simple
 
 main :: IO ()
-main = someFunc
+main = do
+    response <- httpLBS "http://httpbin.org/get"
+
+    putStrLn $ "The status code was: " ++
+               show (getResponseStatusCode response)
+    print $ getResponseHeader "Content-Type" response
+    L8.putStrLn $ getResponseBody response
