0973fc50eec58f79b92b15e8d64242efb1785992
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-15 11:16:18 +0000

    I can read and parse a .txt file

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,17 @@ module Main where
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           Network.HTTP.Simple
+import Data.List.Split
 
 main :: IO ()
+--main = do
+--    response <- httpLBS "http://httpbin.org/get"
+--
+--    putStrLn $ "The status code was: " ++
+--               show (getResponseStatusCode response)
+--    print $ getResponseHeader "Content-Type" response
+--    L8.putStrLn $ getResponseBody response
 main = do
-    response <- httpLBS "http://httpbin.org/get"
-
-    putStrLn $ "The status code was: " ++
-               show (getResponseStatusCode response)
-    print $ getResponseHeader "Content-Type" response
-    L8.putStrLn $ getResponseBody response
+    list <- readFile("src/table.txt")
+    putStrLn list
+    print $ splitOn "|" list
